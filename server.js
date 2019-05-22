c5632b1fd2a11539d8828be010cbbbefd9fde179
Nicolas Toulemont <nicolahs.toulemont@gmail.com>
2019-05-22 14:27:56 +0200

    force the server to wait for the DB connection before starting

--- server.js
+++ server.js
@@ -8,37 +8,41 @@ const Loaders = require('./utils/DataLoaders');
 const connectDB = require('./config/db');
 const { AuthUser } = require('./utils/user/auth');
 
-const app = express();
+const startServer = async () => {
+	const app = express();
 
-connectDB();
+	await connectDB();
 
-const server = new ApolloServer({
-	schema,
-	context: ({ req }) => ({
-		user: AuthUser(req),
-		models,
-		Loaders
-	}),
-	formatError: error => ({
-		path: error.path,
-		message: error.message,
-		code: error.extensions.code
-	}),
-	playground: true
-});
-
-// Server static assets in prod
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-	// Serve html file
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	const server = new ApolloServer({
+		schema,
+		context: ({ req }) => ({
+			user: AuthUser(req),
+			models,
+			Loaders
+		}),
+		formatError: error => ({
+			path: error.path,
+			message: error.message,
+			code: error.extensions.code
+		}),
+		playground: true
 	});
-}
 
-server.applyMiddleware({ app });
+	// Server static assets in prod
+	if (process.env.NODE_ENV === 'production') {
+		// Set static folder
+		app.use(express.static('client/build'));
+		// Serve html file
+		app.get('*', (req, res) => {
+			res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		});
+	}
 
-const PORT = process.env.PORT || 4000;
+	server.applyMiddleware({ app });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/graphql?`));
+	const PORT = process.env.PORT || 4000;
+
+	app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/graphql?`));
+};
+
+startServer();
